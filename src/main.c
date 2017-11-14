d96a52098da785bf83c479f102bd94c63f029bfd
Thomas Biddulph <462767@hull.ac.uk>
2017-11-14 11:58:21 +0000

    added main.c file

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: 462767
+ *
+ * Created on 14 November 2017, 11:58
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
--- src/nbproject/Package-default.bash
+++ src/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/08206-Project.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=08206-Project.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=08206-project/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/src.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=src.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=src/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/08206-project/bin
+makeDirectory ${TMPDIR}/src/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/08206-project.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/src.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/08206-project.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/src.tar *
 checkReturnCode
 
 # Cleanup
